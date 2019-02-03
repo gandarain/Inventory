5fe8d81f5b99960093290727dffc251ccfaf43b4
Ajhoel Tragedy <panduwiranata43@gmail.com>
2019-02-04 00:36:58 +0700

    Update Model User  - add new function (to get user groups

--- application/models/M_user.php
+++ application/models/M_user.php
@@ -32,4 +32,30 @@ class M_user extends CI_Model
 
         return array(true, $query);
     }
+
+    public function get_user_groups($params = array())
+    {
+        $this->db->select("
+            a.username, a.name, a.phone, a.email,
+            b.user_id, b.group_id, b.id ug_id,
+            c.name group_name, c.special_privilege, c.dashboard_access,
+            c.description group_desc
+            ");
+        $this->db->from('users a');
+        $this->db->join('users_group b', 'b.user_id = a.id');
+        $this->db->join('groups c', 'c.id = b.group_id');
+
+        if(!empty($params['username']))
+            $this->db->where('a.username', $params['username']);
+        if(!empty($params['id']))
+            $this->db->where('a.id', $params['id']);
+        if(!empty($params['user_id']))
+            $this->db->where('b.user_id', $params['user_id']);
+        if(!empty($params['group_id']))
+            $this->db->where('b.group_id', $params['group_id']);
+
+        $query = $this->db->get();
+
+        return $query->result();
+    }
 }
