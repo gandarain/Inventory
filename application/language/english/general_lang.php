58d396cc0ad717660d6e09c518e9bc9bb509fce2
Ajhoel Tragedy <panduwiranata43@gmail.com>
2019-02-04 00:36:23 +0700

    Update Language

--- application/language/english/general_lang.php
+++ application/language/english/general_lang.php
@@ -6,12 +6,15 @@ $lang['appname'] = 'My System Name';
 //****** Aa *****//
 //****** Bb *****//
 //****** Cc *****//
+//****** Dd *****//
+$lang['dialog_logout'] = 'Are you sure want to logout?';
 //****** Ff *****//
 //****** Gg *****//
 $lang['greetings_login'] = 'Welcome back <strong>%s</strong><br>';
 //****** Ll *****//
 $lang['login'] = 'Login';
 $lang['login_form'] = 'Login Form';
+$lang['logout'] = 'Logout';
 //****** Mm *****//
 $lang['msg_login_invalid'] = 'Invalid Username or Password';
 $lang['msg_param_required'] = 'Params %s is required';
