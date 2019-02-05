a91aa5413f503688ac9b24391d113f68169abac8
Ajhoel Tragedy <panduwiranata43@gmail.com>
2019-02-05 12:42:56 +0700

    Update Sidenav

--- application/views/template/default_sidenavs.php
+++ application/views/template/default_sidenavs.php
@@ -22,9 +22,15 @@
 			<div class="menu_section">
 				<h3>General</h3>
 				<ul class="nav side-menu">
+					<li><a href="<?php echo base_url('dashboard') ?>"><i class="fas fa-tachometer-alt"></i> <?php echo lang('dashboard') ?></a></li>
+					<li><a><i class="fas fa-user-secret"></i> <?php echo lang('master') ?> <span class="fa fa-chevron-down"></span></a>
+						<ul class="nav child_menu">
+							<li><a href="<?php echo base_url('user') ?>"><?php echo lang('user') ?></a></li>
+							<li><a href="<?php echo base_url('user/read_type') ?>"><?php echo lang('user_type') ?></a></li>
+						</ul>
+					</li>
 					<li><a><i class="fa fa-edit"></i> Home <span class="fa fa-chevron-down"></span></a>
 						<ul class="nav child_menu">
-							<li><a href="<?php echo base_url('main') ?>">Dashboard</a></li>
 							<li><a href="<?php echo base_url('main/simple_template') ?>">Simple Page</a></li>
 						</ul>
 					</li>
