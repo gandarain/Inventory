552b8b12bce7da7f1f0a8684c9d16c33092d450a
Ajhoel Tragedy <panduwiranata43@gmail.com>
2019-02-05 12:43:02 +0700

    Update Language

--- application/language/english/general_lang.php
+++ application/language/english/general_lang.php
@@ -4,19 +4,32 @@ $lang['appdev'] = '&copy;2019 All Rights Reserved. Ajhoel Tragedy';
 $lang['appname'] = 'My System Name';
 
 //****** Aa *****//\
+$lang['action'] = 'Action';
 $lang['address'] = 'Address';
+
 //****** Bb *****//
 $lang['birth'] = 'Birthdate';
-$lang['btn_submit'] = 'Submit';
+$lang['btn_cancel'] = 'Abandon Changes';
+$lang['btn_save'] = 'Save';
 $lang['btn_search'] = 'Search';
+$lang['btn_submit'] = 'Submit';
+$lang['btn_update'] = 'Save Changes';
+
 //****** Cc *****//
+$lang['code'] = 'Code';
 $lang['create_account'] = 'Create Account';
+
 //****** Dd *****//
+$lang['dashboard'] = 'Dashboard';
+$lang['dialog_abandon_changes'] = 'Are you sure want to leave this operation?';
+$lang['dialog_delete'] = 'Are you sure want to delete this?';
 $lang['dialog_login'] = 'Already a member?';
 $lang['dialog_logout'] = 'Are you sure want to logout?';
 $lang['dialog_signup'] = 'New to site?';
+
 //****** Ee *****//
 $lang['email'] = 'Email';
+
 //****** Ff *****//
 $lang['forgot_password'] = 'Lost your password?';
 $lang['form_login'] = 'Login Form';
@@ -24,14 +37,26 @@ $lang['form_register'] = 'Registration Form';
 
 //****** Gg *****//
 $lang['greetings_login'] = 'Welcome back <strong>%s</strong><br>';
+$lang['greetings_select'] = '- Please Select -';
 $lang['greetings_signup'] = 'Congratulations you are registered %s';
 
 //****** Ll *****//
+$lang['list'] = 'List Data';
 $lang['login'] = 'Log In';
 $lang['logout'] = 'Log Out';
+
 //****** Mm *****//
+$lang['master'] = 'Master';
 $lang['msg_login_invalid'] = 'Invalid Username or Password';
+$lang['msg_noaccess'] = 'You cannot access this website';
+$lang['msg_nopermission_menu'] = 'You have no Permission';
+$lang['msg_op_create'] = "to perform <strong>Create</strong> Operation";
+$lang['msg_op_delete'] = "to perform <strong>Delete</strong> Operation";
+$lang['msg_op_read'] = "to perform <strong>Read</strong> Operation";
+$lang['msg_op_report'] = "to perform <strong>Read Report</strong>";
+$lang['msg_op_update'] = "to perform <strong>Edit</strong> Operation";
 $lang['msg_param_required'] = 'Params %s is required';
+
 //****** Nn *****//
 $lang['name'] = 'Name';
 $lang['name_full'] = 'Full Name';
@@ -40,8 +65,12 @@ $lang['name_nick'] = 'Nick Name';
 //****** Pp *****//
 $lang['password'] = 'Password';
 $lang['phone'] = 'Phone';
+
 //****** Ss *****//
+$lang['search'] = 'Search';
 $lang['something_went_wrong'] = 'Oops, Something went wrong :(. %s';
 
 //****** Uu *****//
+$lang['user'] = 'User';
 $lang['username'] = 'Username';
+$lang['user_type'] = 'User Type';
