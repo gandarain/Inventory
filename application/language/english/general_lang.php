dfcabe941de66a1b9cc26c27da7f986e5e8b432e
Ajhoel Tragedy <panduwiranata43@gmail.com>
2019-02-05 11:33:33 +0700

    Update Language

--- application/language/english/general_lang.php
+++ application/language/english/general_lang.php
@@ -3,23 +3,43 @@
 $lang['appdev'] = '&copy;2019 All Rights Reserved. Ajhoel Tragedy';
 $lang['appname'] = 'My System Name';
 
-//****** Aa *****//
+//****** Aa *****//\
+$lang['address'] = 'Address';
 //****** Bb *****//
+$lang['birth'] = 'Birthdate';
+$lang['btn_submit'] = 'Submit';
+$lang['btn_search'] = 'Search';
 //****** Cc *****//
+$lang['create_account'] = 'Create Account';
 //****** Dd *****//
+$lang['dialog_login'] = 'Already a member?';
 $lang['dialog_logout'] = 'Are you sure want to logout?';
+$lang['dialog_signup'] = 'New to site?';
+//****** Ee *****//
+$lang['email'] = 'Email';
 //****** Ff *****//
+$lang['forgot_password'] = 'Lost your password?';
+$lang['form_login'] = 'Login Form';
+$lang['form_register'] = 'Registration Form';
+
 //****** Gg *****//
 $lang['greetings_login'] = 'Welcome back <strong>%s</strong><br>';
+$lang['greetings_signup'] = 'Congratulations you are registered %s';
+
 //****** Ll *****//
-$lang['login'] = 'Login';
-$lang['login_form'] = 'Login Form';
-$lang['logout'] = 'Logout';
+$lang['login'] = 'Log In';
+$lang['logout'] = 'Log Out';
 //****** Mm *****//
 $lang['msg_login_invalid'] = 'Invalid Username or Password';
 $lang['msg_param_required'] = 'Params %s is required';
+//****** Nn *****//
+$lang['name'] = 'Name';
+$lang['name_full'] = 'Full Name';
+$lang['name_nick'] = 'Nick Name';
+
 //****** Pp *****//
 $lang['password'] = 'Password';
+$lang['phone'] = 'Phone';
 //****** Ss *****//
 $lang['something_went_wrong'] = 'Oops, Something went wrong :(. %s';
 
