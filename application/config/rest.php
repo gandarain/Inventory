894f9028e76bf901b8e58da4a8bc42a996f361cf
Ajhoel Tragedy <panduwiranata43@gmail.com>
2019-02-08 11:39:56 +0700

    Update Rest Config

--- application/config/rest.php
+++ application/config/rest.php
@@ -536,7 +536,7 @@ $config['rest_language'] = 'english';
 | will access it through a browser
 |
 */
-$config['check_cors'] = FALSE;
+$config['check_cors'] = TRUE;
 
 /*
 |--------------------------------------------------------------------------
@@ -580,7 +580,7 @@ $config['allowed_cors_methods'] = [
 | source domain
 |
 */
-$config['allow_any_cors_domain'] = FALSE;
+$config['allow_any_cors_domain'] = TRUE;
 
 /*
 |--------------------------------------------------------------------------
